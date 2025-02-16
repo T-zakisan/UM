4ef9587dafc354a0af0198ca96eaf95781c930a5
T-zakisan <43605763+T-zakisan@users.noreply.github.com>
2025-02-17 05:33:06 +0900

    Update makeTable.py

--- makeTable.py
+++ makeTable.py
@@ -3,10 +3,7 @@
 #   Excelの表をTeX(longtable)形式に変換
 #   2025.02.17
 #   <issu>
-#    - longtableのヘッダ，nextheadが未設定
-#    - インデントの有無に対して，p{長さ}の処理が未実装 
 #    - |で\hlineが未実装
-#    - コメントがおおよそつけていない（勢いで作ったので・・・）
 #    - 関数で小分け（ヘッダ，リスト＆フッタ）程度に小分けにしたいかな
 ##############################################################
 
@@ -30,123 +27,131 @@ def create_table_code(sheet):
     myStr = "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n" + \
             "% ファイル名：" + output_file_path + "\n" \
             "% 内　　　容：" + sheet['A1'].value + "\n" \
+            "% 製　　　作：2025.02.18\n" \
             "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n\n"
-    f.write( myStr )
-
+    f.write( myStr ) # テキスト出力
 
     # 表の設定：インデント含む
-    indent = [ "0mm", 156.1 ] # 初期値：インデントなし
+    indent = "0mm" # 初期値：インデントなし
     if "あり" in sheet['A3'].value:
-      indent = [ "\\myLEFTSKIP", 140.0 ] # インデントあり時
+      indent = f"\\myLEFTSKIP" # インデントあり時
     myStr = f"\\setlength{{\\tabcolsep}}{{2mm}} % セルの余白\n" \
-            f"\\setlength\\LTleft{{{ indent[0] }}} % 字下げ\n" \
+            f"\\setlength\\LTleft{{{ indent }}} % 字下げ\n" \
             f"\\setlength\\LTright{{0pt}} % 右側余白設定\n" \
             f"\\setlength\\LTpre{{2mm}} % 表前の余白\n" \
             f"\\setlength\\LTpost{{0mm}} % 表後の余白\n"
-    f.write( myStr )
+    f.write( myStr ) # テキスト出力
 
 
     # ヘッダ部
     myLength = 0
     myStr = f"\\begin{{longtable}}"
     myStr += "{"
-    for row in sheet.iter_rows(min_row=4, max_row=4):
-      for idx, cell in enumerate( row[0:] ): 
+    for row in sheet.iter_rows(min_row=4, max_row=4): #
+      for idx, cell in enumerate( row[0:] ): # 
         if idx>=2 :
 
           # 背景色
-          tmp = cell.offset(2,0).value
+          tmp = cell.offset(2,0).value # 背景値取得
           myStr += " >{"
-          if tmp != None:
+          if not( tmp is None ): # 背景色（濃度）：入力あり
             myStr += f"\\columncolor{{gray!{tmp}}}"
 
           # 寄せ
-          tmp = cell.offset(1,0).value
-          if tmp == "l":
+          tmp = cell.offset(1,0).value # 寄せ取得
+          if tmp == "l": # 左寄せ
             myStr += f"\\raggedright\\arraybackslash"
-          elif tmp == "c" or tmp == "":
+          elif tmp == "c" or tmp == "": # 中央寄せ：未記入
             myStr += f"\\centering\\arraybackslash"
-          elif tmp == "r":
+          elif tmp == "r": # 右寄せ
             myStr += f"\\raggedleft\\arraybackslash"
           myStr += "}"
 
           # 列幅
-          tmp = f"{cell.offset(0,0).value}"
-          if tmp != "*":
-            myStr += f"p{{{tmp}mm}}"
-            # myLength += tmp
+          tmp = f"{cell.offset(0,0).value}" # 列幅取得
+          if tmp.isdigit(): # 数値の場合
+            myStr += f"p{{{tmp}mm}}" # 数値をそのまま反映
+            myLength += int( tmp ) + 4 
           else:
-            if "あり" in sheet['A3'].value:
-              pass
-              # mystr += f"p{{{tmp}}}" # 修正必要
-            else:
-              pass
-              # mystr += f"p{{{tmp}}}" # 修正必要
+            myStr += f"p{{\\myTableWidth}}" # 修正必要
+            pass
     myStr += "}\n"
-    f.write( myStr )
+
+    tmp = f"\\newlength{{\\myTableWidth}}\n" \
+          f"\\setlength{{\\myTableWidth}}{{166mm - {myLength}mm - {indent} }}\n"
+    f.write( tmp ) # テキス出力
+    f.write( myStr ) # テキス出力
 
 ##########################################################
 
 
-    # リスト部分
-    myStr = ""
-    count_row = 0
+    # リスト＆環境の締め部
+    myStr = "" # テキストの一時保管用
+    count_row = 0 # 行カウンタ：偶/奇数処理用
     for row in sheet.iter_rows(min_row=7): # 行で繰り返し
-      list = []
-      # list.clear
-      count_row += 1
-      flag = False
+      list = [] # 行項目リスト
+      count_row += 1 # 行カウンタの加算
+      flag_merge = False # 結合フラグ
+      flag_header = False # ヘッダ用フラグ
       for ii, cell in enumerate( row[1:] ): # 各行の列で繰り返し(列Bスタート)
         
         # 行指定の背景色
-        if ii == 0:
-          if cell.value is None:
-            if ( count_row % 2 ) == 0:
+        if ii == 0: # 列B @ Excel
+          if cell.value is None: # 列Bの値が空白の場合
+            if ( count_row % 2 ) == 0: # 偶奇数判定
               myStr += f"\\rowcolor{{gray!10}}\t" # 偶数行
-            else:
+            else: 
               myStr += f"\\rowcolor{{white}}\t" # 奇数行
           else:
-            if str(cell.value).isdigit():
-              myStr += f"\\rowcolor{{gray!{cell.value}}}\t" # 値がある場合
-            else:
-              pass
+            if str(cell.value).isdigit(): # 数値判定
+              myStr += f"\\rowcolor{{gray!{cell.value}}}\t" # 背景色
+              if cell.value == 60: # ヘッダフラグ
+                flag_header = True # 
 
         # 結合チェック
-        if (ii > 0) and (ii<len(row)):
-          flag = False
-          for jj in range( len(list) ):
-            if cell.value == list[jj][0] and not(list[jj][0] is None):
-              flag = True
+        if (ii > 0) and (ii<len(row)): # 不要項目除去
+          flag_merge = False # 結合フラグ
+          for jj in range( len(list) ): # 既存リストで繰り返し
+            if cell.value == list[jj][0] and not(list[jj][0] is None): # 結合判定
+              flag_merge = True # 結合（隣セルと同値）：あり
 
-          if flag == True:
-            list[jj][1] += 1              
-          else:
-            list.append([cell.value,1])
+          if flag_merge == True: # 結合（隣セルと同値）：あり
+            list[jj][1] += 1 # 結合数をカウントアップ
+          else: # 結合：なし
+            list.append([cell.value,1]) # リストにセル値，数量(1)を追記
 
       for ii, ll in enumerate( list ):
-        if ll[1] == 1: # セル結合なし
-          if ll[0] is None:
-            myStr += f" & "
-          else:
-            if not( row[0].value is None): # 色あり
-              myStr += f"\\textbf{{{ll[0]}}} & "
-            else:
-              myStr += f"{ll[0]} & "
+        if ll[1] == 1: # セル結合なし(1=1セル) 
+          if ll[0] is None: # セル値：なし
+            myStr += f" & " # 空白表示
+          else: # セル値：あり
+            if not( row[0].value is None): # 行背景色：あり
+              print( ll[0] )
+              if "|" in ll[0]:
+                myStr += str( ll[0].replace("|", "\\hline") )
+              else: 
+                myStr += f"\\textbf{{{ll[0]}}} & " # 太文字
+
+            else:# 行背景色：なし
+              myStr += f"{ll[0]} & " # そのまま表示
         
         else: # セル結合あり
           if ii == 0: # 列C
             align = "l" # B列：左寄＆太文字
             myStr += f"\\multicolumn{{{ll[1]}}}{{{align}}}{{\\textbf{{{ll[0]}}}}} & " # セル値：太文字
+            count_row = 0 # 行カウンタの初期化
           else: # 列D以降
             align = "c" # 中央寄せ
             myStr += f"\\multicolumn{{{ll[1]}}}{{{align}}}{{{ll[0]}}} & " # セル値
 
-
-
       myStr = myStr[:-2]  # 最後の&除去
       myStr += f"\\\\ \n" # 改行
+      if flag_header == True:
+        flag_header = False
+        tmp = myStr
+        myStr = f"{tmp} \\endfirsthead  \\hline\\hline \n" \
+                f"{tmp} \\endhead       \\hline\\hline \n" 
 
-
-    myStr += f"\\hline\\hline \n"
-    myStr += f"\\end{{longtable}}"
+    myStr += f"\\hline\\hline \n" # 最後の罫線と改行
+    myStr += f"\\end{{longtable}}" # 環境の締め
     f.write( myStr )
