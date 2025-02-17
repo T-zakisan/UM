be4fe21e5ef4644182b54c5037399f7ed5b6b108
T-zakisan <43605763+T-zakisan@users.noreply.github.com>
2025-02-18 05:42:05 +0900

    Update makeValuable.py

--- makeValuable.py
+++ makeValuable.py
@@ -14,14 +14,9 @@ def create_variable_definitions( sheet ):
     for idx, row in enumerate( sheet.iter_rows(min_row=2) ):
       col_a_value = row[0].value  # [列A]変数名
       col_b_value = row[1].value  # [列B]設定値
-      col_d_value = row[3].value  # [列D]備考
-      
+      col_d_value = row[3].value  # [列D]備考      
       myVariable = f"\変数：{col_a_value}"
-      sheet.cell( row=idx+2, column=3, value=myVariable )
-      print( f"{idx+2} : {myVariable}" )
-
 
       if col_a_value is not None and col_b_value is not None:
         col_b_value = utils.escape_tex_string(col_b_value) # テキストチェック(エスケープ文字対応)
         f.write(f"\\newcommand{{{myVariable}}}{{{col_b_value}}} % {col_d_value }\n")
-
